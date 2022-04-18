b9403a835cfa9b263e1e23be79465e1713c16c09
Ihor Bilyk <bilyk.box@gmail.com>
2022-04-18 13:06:28 +0300

    update

--- newsfeed/bot.py
+++ newsfeed/bot.py
@@ -4,6 +4,7 @@ import requests
 
 from newsfeed import config
 import newsfeed.utils.db as db_utils
+from time import sleep
 
             
 def send_news():
@@ -11,12 +12,11 @@ def send_news():
     last_id = db_utils.select_last_id()
     news = db_utils.select_news(last_id)
     
-    conn = db_utils.create_mysql_connector(config.db_news)
-    db_utils.insert_records_to_table("variables",[{'name':'last_id','value':last_id}],'name', conn)
-    
     if news.shape[0]>0:
         last_id = news.id.max()
         logging.info('Writing data to channel')
+        conn = db_utils.create_mysql_connector(config.db_news)
+        db_utils.insert_records_to_table("variables",[{'name':'last_id','value':last_id}],'name', conn)
         
         failed=False
         for ix,row in news.iterrows():
@@ -27,6 +27,7 @@ def send_news():
                 logging.error(f'Failed to send mgs \n {text}')
                 logging.error(query)
                 failed=True
+            sleep(1)
         
         if failed:
             sys.exit(1) 
